4c9cb6af212d29e2b547c7a652682bc616adf7ec
truekendor <sadqwerty000@gmail.com>
2026-05-02 17:06:06 +0300

    tweak tsify derive and squareStr enum

--- src/helpers/tsify.rs
+++ src/helpers/tsify.rs
@@ -97,14 +97,13 @@ pub enum ColorChar {
 }
 
 // copy of a shakmaty's Square enum for tsify types
-// #[derive(tsify::Tsify, Serialize, Deserialize, Debug, EnumString, IntoStaticStr)]
 #[rustfmt::skip]
-#[derive(tsify::Tsify, Serialize, Deserialize, Debug, Display, EnumString, IntoStaticStr)]
+#[derive(tsify::Tsify, Serialize, Deserialize, Debug, Display, EnumString, IntoStaticStr, PartialEq, Eq)]
 #[tsify(into_wasm_abi, from_wasm_abi)]
 #[serde(rename_all = "camelCase")]
 #[strum(serialize_all = "camelCase")]
 pub enum SquareStr {
-    A1, B1, C1, D1, E1, F1, G1, H1,
+    A1 = 1, B1, C1, D1, E1, F1, G1, H1,
     A2, B2, C2, D2, E2, F2, G2, H2,
     A3, B3, C3, D3, E3, F3, G3, H3,
     A4, B4, C4, D4, E4, F4, G4, H4,
