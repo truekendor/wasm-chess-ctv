5779e06f3dbc5b22f0bb729deee264e91b1f9c67
truekendor <sadqwerty000@gmail.com>
2026-05-07 04:25:32 +0300

    port more chess.js tests

--- src/tests/move_tests.rs
+++ src/tests/move_tests.rs
@@ -1,6 +1,9 @@
 #[cfg(test)]
 pub mod move_tests {
-    use crate::{WasmChess, tsify_structs::PieceSymbol};
+    use crate::{
+        WasmChess,
+        tsify_structs::{PieceSymbol, SquareStr},
+    };
 
     #[test]
     fn move_works_standard_algebraic_notation() {
@@ -52,6 +55,7 @@ pub mod move_tests {
         pretty_assertions::assert_eq!(chess.fen(None), fen_next);
 
         pretty_assertions::assert_eq!(rook_mate_move.is_en_passant, false);
+        pretty_assertions::assert_eq!(rook_mate_move.is_regular_capture, false);
         pretty_assertions::assert_eq!(rook_mate_move.is_big_pawn, false);
 
         pretty_assertions::assert_eq!(rook_mate_move.is_castle, false);
@@ -88,6 +92,7 @@ pub mod move_tests {
         pretty_assertions::assert_eq!(chess.fen(None), fen_next);
 
         pretty_assertions::assert_eq!(rook_mate_move.is_en_passant, true);
+        pretty_assertions::assert_eq!(rook_mate_move.is_regular_capture, false);
         pretty_assertions::assert_eq!(rook_mate_move.is_big_pawn, false);
 
         pretty_assertions::assert_eq!(rook_mate_move.is_castle, false);
@@ -124,6 +129,7 @@ pub mod move_tests {
         pretty_assertions::assert_eq!(chess.fen(None), fen_next);
 
         pretty_assertions::assert_eq!(rook_mate_move.is_en_passant, true);
+        pretty_assertions::assert_eq!(rook_mate_move.is_regular_capture, false);
         pretty_assertions::assert_eq!(rook_mate_move.is_big_pawn, false);
 
         pretty_assertions::assert_eq!(rook_mate_move.is_castle, false);
@@ -137,6 +143,78 @@ pub mod move_tests {
 
         chess.undo();
 
+        pretty_assertions::assert_eq!(
+            chess.zobrist_hash(),
+            WasmChess::new(Some(fen)).unwrap().zobrist_hash()
+        );
+    }
+
+    #[test]
+    fn move_works_standard_algebraic_notation_pin_disambiguates_piece() {
+        let fen = "r2qkbnr/ppp2ppp/2n5/1B2pQ2/4P3/8/PPP2PPP/RNB1K2R b KQkq - 3 7".to_string();
+        let fen_next = "r2qkb1r/ppp1nppp/2n5/1B2pQ2/4P3/8/PPP2PPP/RNB1K2R w KQkq - 4 8".to_string();
+
+        let mut chess = WasmChess::new(Some(fen.clone())).unwrap();
+
+        let mov = chess.make_move("Ne7").unwrap();
+
+        pretty_assertions::assert_eq!(mov.from, SquareStr::G8);
+        pretty_assertions::assert_eq!(mov.to, SquareStr::E7);
+        pretty_assertions::assert_eq!(mov.piece, PieceSymbol::N);
+
+        pretty_assertions::assert_eq!(&mov.after, &fen_next);
+        pretty_assertions::assert_eq!(&mov.after, &chess.fen(None));
+        pretty_assertions::assert_eq!(mov.is_regular_capture, false);
+        pretty_assertions::assert_eq!(mov.promotion.is_some(), false);
+
+        pretty_assertions::assert_eq!(mov.is_en_passant, false);
+        pretty_assertions::assert_eq!(mov.is_big_pawn, false);
+
+        pretty_assertions::assert_eq!(mov.is_castle, false);
+        pretty_assertions::assert_eq!(mov.is_kingside_castle, false);
+        pretty_assertions::assert_eq!(mov.is_queenside_castle, false);
+
+        pretty_assertions::assert_eq!(
+            chess.zobrist_hash(),
+            WasmChess::new(Some(fen_next)).unwrap().zobrist_hash()
+        );
+        chess.undo();
+        pretty_assertions::assert_eq!(
+            chess.zobrist_hash(),
+            WasmChess::new(Some(fen)).unwrap().zobrist_hash()
+        );
+    }
+
+    #[test]
+    fn move_works_standard_algebraic_overly_disabig_piece() {
+        let fen = "r2qkbnr/ppp2ppp/2n5/1B2pQ2/4P3/8/PPP2PPP/RNB1K2R b KQkq - 3 7".to_string();
+        let fen_next = "r2qkb1r/ppp1nppp/2n5/1B2pQ2/4P3/8/PPP2PPP/RNB1K2R w KQkq - 4 8".to_string();
+
+        let mut chess = WasmChess::new(Some(fen.clone())).unwrap();
+
+        let mov = chess.make_move("Nge7").unwrap();
+
+        pretty_assertions::assert_eq!(mov.from, SquareStr::G8);
+        pretty_assertions::assert_eq!(mov.to, SquareStr::E7);
+        pretty_assertions::assert_eq!(mov.piece, PieceSymbol::N);
+
+        pretty_assertions::assert_eq!(&mov.after, &fen_next);
+        pretty_assertions::assert_eq!(&mov.after, &chess.fen(None));
+        pretty_assertions::assert_eq!(mov.is_regular_capture, false);
+        pretty_assertions::assert_eq!(mov.promotion.is_some(), false);
+
+        pretty_assertions::assert_eq!(mov.is_en_passant, false);
+        pretty_assertions::assert_eq!(mov.is_big_pawn, false);
+
+        pretty_assertions::assert_eq!(mov.is_castle, false);
+        pretty_assertions::assert_eq!(mov.is_kingside_castle, false);
+        pretty_assertions::assert_eq!(mov.is_queenside_castle, false);
+
+        pretty_assertions::assert_eq!(
+            chess.zobrist_hash(),
+            WasmChess::new(Some(fen_next)).unwrap().zobrist_hash()
+        );
+        chess.undo();
         pretty_assertions::assert_eq!(
             chess.zobrist_hash(),
             WasmChess::new(Some(fen)).unwrap().zobrist_hash()
