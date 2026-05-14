6ac1aba1dc13cdb0a9bc519565446a2229a5a5b8
truekendor <sadqwerty000@gmail.com>
2026-05-14 22:55:50 +0300

    upd

--- src/impls/impl_queries.rs
+++ src/impls/impl_queries.rs
@@ -1,3 +1,5 @@
+use shakmaty::{Role, uci::UciMove};
+
 use super::*;
 
 #[wasm_bindgen]
@@ -236,7 +238,24 @@ impl WasmChess {
         &self,
         filter_options: Option<LegalMovesFilterOptions>,
     ) -> Vec<MoveString> {
-        helpers::legal_moves::uci(&self.chess, filter_options)
+        let filter_options = unwrap_filter_options(&filter_options);
+
+        let legal_moves: Vec<String> = self
+            .chess
+            .legal_moves()
+            .iter()
+            .filter_map(|mov| {
+                let move_filtered = filter_move(mov, &filter_options);
+                if move_filtered {
+                    return None;
+                }
+
+                let uci_move = UciMove::from_move(*mov, shakmaty::CastlingMode::Chess960);
+                return Some(uci_move.to_string());
+            })
+            .collect();
+
+        legal_moves
     }
 
     #[wasm_bindgen(js_name = "legalMovesSan")]
@@ -244,7 +263,30 @@ impl WasmChess {
         &self,
         filter_options: Option<LegalMovesFilterOptions>,
     ) -> Vec<MoveString> {
-        helpers::legal_moves::san(&self.chess, filter_options)
+        let filter_options = unwrap_filter_options(&filter_options);
+
+        let legal_moves: Vec<String> = self
+            .chess
+            .legal_moves()
+            .iter()
+            .filter_map(|mov| {
+                let mut chess_clone = self.chess.clone();
+
+                let move_filtered = filter_move(mov, &filter_options);
+                if move_filtered {
+                    return None;
+                }
+
+                let san_move = San::from_move(&self.chess, *mov);
+                chess_clone.play_unchecked(*mov);
+
+                let san_str = san_to_san_plus(&san_move, &chess_clone);
+
+                return Some(san_str);
+            })
+            .collect();
+
+        legal_moves
     }
 
     /// # API Discrepancy: chess.js Compatibility
@@ -262,7 +304,25 @@ impl WasmChess {
         &self,
         filter_options: Option<LegalMovesFilterOptions>,
     ) -> Vec<MoveVerbose> {
-        helpers::legal_moves::verbose(&self.chess, filter_options)
+        let filter_options = unwrap_filter_options(&filter_options);
+
+        let legal_moves: Vec<MoveVerbose> = self
+            .chess
+            .legal_moves()
+            .iter()
+            .filter_map(|raw_move| {
+                let move_filtered = filter_move(raw_move, &filter_options);
+                if move_filtered {
+                    return None;
+                }
+
+                let move_verbose = verbose_move_from_raw_move(*raw_move, &self.chess);
+
+                Some(move_verbose)
+            })
+            .collect();
+
+        legal_moves
     }
 
     #[wasm_bindgen(js_name = "getCastlingRights")]
@@ -295,4 +355,62 @@ impl WasmChess {
     pub fn zobrist_hash(&self) -> u64 {
         return self.hash.0;
     }
+}
+
+struct FilterOptions {
+    pub square: Option<Square>,
+    pub piece: Option<Role>,
+}
+
+fn unwrap_filter_options(options: &Option<LegalMovesFilterOptions>) -> FilterOptions {
+    let filter_square_option: Option<Square> = match options.as_ref() {
+        Some(val) => {
+            if let Some(square) = &val.from_square {
+                Some(square.to_shakmaty_sq())
+            } else {
+                None
+            }
+        }
+        None => None,
+    };
+
+    let filter_piece_option: Option<Role> = match options.as_ref() {
+        Some(val) => {
+            let p = val.piece.as_ref();
+
+            if let Some(oo) = p {
+                Some(oo.to_shakmaty_piece_role())
+            } else {
+                None
+            }
+        }
+        None => None,
+    };
+
+    FilterOptions {
+        square: filter_square_option,
+        piece: filter_piece_option,
+    }
+}
+
+fn filter_move(mov: &Move, options: &FilterOptions) -> bool {
+    if let Some(filter_square) = options.square {
+        let from = mov.from();
+
+        let Some(from) = from else {
+            return true;
+        };
+
+        if filter_square != from {
+            return true;
+        }
+    }
+
+    if let Some(filter_piece) = options.piece {
+        if mov.role() != filter_piece {
+            return true;
+        }
+    }
+
+    false
 }
